045e1c37ffa98ad2ed16013abd8ab38ecfc1622e
VirginiaRool <virginia.aro@gmail.com>
2022-08-09 22:36:12 -0500

    Added script js

---
+++ src/script.js
@@ -0,0 +1,6 @@
+let apiKey = "78af43c4b86f6fe747e2eeb5803e94cd";
+let city = "New York";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=${units}`;
+
+console.log(apiUrl);
