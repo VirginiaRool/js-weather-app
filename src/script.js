16930316ff034b0d564637c6f7b1c661ba6ad289
VirginiaRool <virginia.aro@gmail.com>
2022-08-11 00:13:52 -0500

    updated api key

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
-let apiKey = "78af43c4b86f6fe747e2eeb5803e94cd";
-let city = "New York";
+let apiKey = "c7cd54d7f273febb5e037c3f52d9dfdb";
+let city = "London,uk";
 let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=${units}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
 console.log(apiUrl);
