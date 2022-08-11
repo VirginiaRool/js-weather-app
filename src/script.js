ad7658eb6e5d1fac85b0380f5233995b412dfd37
VirginiaRool <virginia.aro@gmail.com>
2022-08-11 18:14:12 -0500

    Updated script

--- src/script.js
+++ src/script.js
@@ -1,6 +1,14 @@
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+}
+
 let apiKey = "c7cd54d7f273febb5e037c3f52d9dfdb";
 let city = "London,uk";
 let units = "metric";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
