0cbb7d5b00e92499597cf3fe951e8958cf34752f
VirginiaRool <virginia.aro@gmail.com>
2022-08-23 17:56:54 -0500

    Added the Search engine

--- src/script.js
+++ src/script.js
@@ -43,9 +43,19 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-let apiKey = "c7cd54d7f273febb5e037c3f52d9dfdb";
-let city = "Los Angeles";
-let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+function search(city) {
+  let apiKey = "c7cd54d7f273febb5e037c3f52d9dfdb";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
