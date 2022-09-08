bad466e9949e6cbb2a78adaf53b78d2f6510cc65
VirginiaRool <virginia.aro@gmail.com>
2022-09-08 14:48:31 -0500

    updated .forEach

--- src/script.js
+++ src/script.js
@@ -23,25 +23,24 @@ function formatDate(timeStamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let dailyForecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
+  dailyForecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `      
         <div class="col-2">
-          <li>${day}</li>
+          <li>${forecastDay.dt}</li>
           <img
-            src="http://openweathermap.org/img/wn/50d@2x.png"
+            src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
             alt=""
             width="40"            
           />
           <li>
-            <span class="forecast-max">32/</span>
-            <span class="forecast-min">15</span>
+            <span class="forecast-max">${forecastDay.temp.max}°</span>
+            <span class="forecast-min">${forecastDay.temp.min}°</span>
           </li>
         </div>
       
